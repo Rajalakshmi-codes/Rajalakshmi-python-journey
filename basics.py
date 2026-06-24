411193724fbedba9918b8645d28f93866e289539
Rajalakshmi-codes <sathishk2569@gmail.com>
2026-06-24 19:26:17 +0530

    feat: day 1 - variables and basics program

---
+++ basics.py
@@ -0,0 +1,9 @@
+name = "Rajalakshmi"
+age = 22
+height = 5.4
+is_student = True
+
+print(f"Hi, I am {name}")
+print(f"I am {age} years old")
+print(f"My height is {height}")
+print(f"Am I a student? {is_student}")
