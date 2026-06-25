397c9499dfde3f589c0c368f09622fddb0dba341
Rajalakshmi-codes <sathishk2569@gmail.com>
2026-06-25 12:48:58 +0530

    feat:day 2:train ticket using if else

---
+++ travel_if.py
@@ -0,0 +1,14 @@
+age=int(input("your age is:"))
+if(age<=5):
+   print("rs . 0")
+   print("childern have no tickets!")
+elif(age>=6 and age<=12):
+   print("rs .50 ")
+   print("as a teenager u have a half of an amount")
+elif(age>=13 and age<=59):
+   print("rs . 150")
+   print("you got the exact price cuz of an adult")
+else:
+   print("rs . 75")
+   print("senior citizen coupen applied !!")
+
