94ecc46fcee1af89c3937dedb87f55cf1082b6cb
Rajalakshmi-codes <sathishk2569@gmail.com>
2026-06-25 13:00:23 +0530

    feat:day2 login system in if else

---
+++ login_if.py
@@ -0,0 +1,6 @@
+username=input("name:")
+password=input("password:")
+if (username == "rajalakshmi" and password == "python123"):
+    print("login successfully!! and welcome back")
+else:
+    print("wrong password, something went wrong//")
