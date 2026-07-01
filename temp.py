5c4bcf99af1b153a018ab7f9c0bead3ea70ec2ce
Rajalakshmi-codes <sathishk2569@gmail.com>
2026-07-01 12:27:19 +0530

    temprature converter

---
+++ temp.py
@@ -0,0 +1,7 @@
+celsius = float(input("Enter Temperature in Celsius: "))
+
+fahrenheit = (celsius * 9 / 5) + 32
+
+print("\n------ Temperature Converter ------")
+print("Celsius:", celsius, "°C")
+print("Fahrenheit:", fahrenheit, "°F")
