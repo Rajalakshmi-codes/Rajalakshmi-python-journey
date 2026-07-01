5062b8afb5f8e4a94ba180b69341cdc4209259dd
Rajalakshmi-codes <sathishk2569@gmail.com>
2026-07-01 12:22:07 +0530

    currency converter

---
+++ currency_converter.py
@@ -0,0 +1,10 @@
+
+amount = float(input("Enter Amount in INR: ₹"))
+exchange_rate = float(input("Enter Exchange Rate: "))
+
+converted_amount = amount * exchange_rate
+
+print("\n------ Currency Converter ------")
+print("Amount (INR): ₹", amount)
+print("Exchange Rate:", exchange_rate)
+print("Converted Amount:", converted_amount)
